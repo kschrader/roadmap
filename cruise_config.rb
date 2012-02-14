cba0642b18237a420bb6359f7bfe25a4d43963f3
Scott Conley <scott@14to9.org>
2012-02-13 23:21:39 -0500

    removing default bundler args

--- cruise_config.rb
+++ cruise_config.rb
@@ -5,4 +5,8 @@ Project.configure do |project|
   project.email_notifier.from = 'development@flipstone.com'
 
   project.build_command = %{rvm 1.9.2 exec bundle exec "unset RUBYOPT BUNDLE_GEMFILE GEM_HOME && cd /home/ubuntu/.cruise/projects/roadmap-qa/work && rvm 1.9.2 exec bundle install && rvm 1.9.2 exec bundle exec rake cruise"}
+
+  # Defaults to '--path=#{project.gem_install_path} --gemfile=#{project.gemfile} --no-color'
+  project.bundler_args = ""
+
 end
