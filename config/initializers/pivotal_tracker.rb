60bc13786869154686e1f2d15dd930bcc6ef3f6b
gardenof <edendelgado@gmail.com>
2012-03-19 13:48:15 -0400

    checks for tracker_token file, puts message if none

--- config/initializers/pivotal_tracker.rb
+++ config/initializers/pivotal_tracker.rb
@@ -1,2 +1,6 @@
-path = Rails.root.join("config/tracker_token")
-PivotalTracker::Client.token = File.read(path).strip
+if File.exists?("config/tracker_token")
+  path = Rails.root.join("config/tracker_token")
+  PivotalTracker::Client.token = File.read(path).strip
+else
+  puts "You need to have a tracker_token file, check Readme"
+end
