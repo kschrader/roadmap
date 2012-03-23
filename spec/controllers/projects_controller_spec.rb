1814433ef27082b4cc37e9ba2e33e8fd84bccbb2
Scott Conley <scott@flipstone.com>
2012-03-23 12:24:41 -0400

    running bilge pump specs on project controller

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,84 +1,13 @@
 require 'spec_helper'
 describe ProjectsController do
- 
-   let (:project) { Factory :project }
+  include BilgePump::Specs
 
-   describe "show" do
-    let (:bundle) { Factory :bundle }
-
-    it "finds a project by :id" do
-      get :show, :id => project.id
-      showed_project = assigns(:project)
-      showed_project.should == project
-    end
-
-    it "returns associated bundles on project" do
-      get :show, :id => bundle.project.id
-      showed_project = assigns(:project)
-      showed_project.bundles.all.should include bundle
-    end
+  def attributes_for_create
+    { name: "Create NAME"}
   end
 
-  describe "edit" do
-    it "works" do
-      get :edit, 
-        id: project.id
-
-      response.should be_success
-      assigns(:project).should == project
-    end
-  end
-
-  describe "update" do
-    it "works" do
-      put :update,
-        id: project.id,
-        project: {
-          name: "OMG I LOVE UPDATE"
-        }
-
-      assigns(:project).name.should == "OMG I LOVE UPDATE"
-      response.should be_redirect
-    end
-  end
-
-  describe "new" do
-    it "works" do
-      get :new
-
-      assigns(:project).should_not be_nil
-      assigns(:project).class.should == Project
-      response.should be_success
-    end
-  end
-
-  describe "create" do
-    it "works" do
-      # POST "/projects"
-      post 'create' , :project => {"name" => "NRD" }
-
-      created_project = Project.find_by_name("NRD")
-      created_project.should_not be_nil
-    end
-  end
-
-  describe "index" do
-    it "works" do
-      get :index
-      assigns(:projects).should include project
-      response.should be_success
-    end
-  end
-
-  describe "delete" do
-    it "works" do
-    delete :destroy,
-        id: project.id
-
-      response.should be_redirect
-      created_project = Project.find_by_name("NRD")
-      created_project.should be_nil
-    end
+  def attributes_for_update
+    { name: "Update NAME" }
   end
 
 end
