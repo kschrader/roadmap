a95640c08a2ea590727f8b3c9bb8ad672a409acc
gardenof <edendelgado@gmail.com>
2012-04-05 13:46:04 -0400

    removed method that called tracker

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,5 @@
 class ProjectsController < ApplicationController
   include BilgePump::Controller
-  before_filter :tracker_projects
 
-  def tracker_projects
-    @tracker_projects = PivotalTracker::Project.all
-  end
 
 end
